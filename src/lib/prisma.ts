2d9498879edd53c477752b13ed29f1905a794731
paruntapa <geniustechmax@gmail.com>
2025-06-22 00:29:38 +0530

    added neonserverless driver: PrismaNeon

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,6 +1,14 @@
-// src/lib/prisma.ts
 import { PrismaClient } from '@prisma/client';
+import { PrismaNeon } from '@prisma/adapter-neon';
 
-const prisma = new PrismaClient();
+const connectionString = process.env.DATABASE_URL;
+
+if (!connectionString) {
+  throw new Error('DATABASE_URL environment variable is not set');
+}
+
+const adapter = new PrismaNeon({ connectionString });
+
+const prisma = new PrismaClient({ adapter });
 
 export default prisma;
